3404b43cc10945a3f826386692f15680bd876bab
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-06 23:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let counterValue = document.getElementById('counter');
+let btn = document.getElementById('incrementBtn');
+function counterFunc (){
+	 let initialValue = parseInt(counterValue.textContent);
+	    alert(initialValue)
+	  counterValue.textContent = initialValue +1;
+	
+	
+}
